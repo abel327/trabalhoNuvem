da929afb013ba4d2259d8c5f20d8200fbcfbe5d5
Your Name <you@example.com>
2025-10-20 20:17:23 -0300

    Arquivo alterao e index adicionado

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express"); //importa o módulo express neste arquivo
+const app = express(); //iniciando o express
+const PORT = process.env.PORT || 8080;
+
+//criando a rota inicial
+app.get("/", function(req,res){
+    res.send("<h1>Bem vindo a tabela de times de futebol!</h1>");
+})
+
+//rota do cadastro de produtos
+app.get("/time", function(req,res){
+    res.send("<h1>Lista de times de futebol!</h1>");
+})
+
+//rota com parametro 
+app.get("/consulta/:parametro", function(req,res){
+    //req --> dados enviados pelo cliente
+    //res --> resposta enviada pelo servidor de volta ao cliente
+    res.send("retorno consulta:" + req.params.parametro);
+})
+
+
+//rota com parametro opcional
+app.get("/cadastro/{:nome}", function(req,res){
+    //req --> dados enviados pelo cliente
+    var nome = req.params.nome;
+    if (nome){
+        res.send("<h1>time " + nome + " criado!</h1>");
+    }else{
+        res.send("time criado!");
+    }
+    
+})
+
+
+//app.listen(process.env.PORT ?? 3000,function(erro){  // cria a aplicação na porta 4000
+//    if (erro){
+//        console.log("Erro ao Iniciar.");
+//    }else{
+//        console.log("Servidor Iniciado.");
+//    }
+//})
+
+console.log ("PORT env: ", process.env.port);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
+
+
